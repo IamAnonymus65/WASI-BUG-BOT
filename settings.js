b911bb63fa56a8ba92f110a1d602dcfef7df250b
IamAnonymus65 <azizazfarmalik@gmail.com>
2024-10-24 15:06:56 +0500

    settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "17784012520"
+global.ownername = "I am Anyonmus"
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
